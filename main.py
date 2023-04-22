63cd9f2a38411dfed2ea347b866b9dac0292513f
yair <yair99@gmail.com>
2023-04-22 22:15:16 +0300

    yuval added negativew prompts

--- main.py
+++ main.py
@@ -32,18 +32,34 @@ app = Client(
 )
 
 
+def slice_positive_negative(string):
+    delimiter = "ng"
+    if delimiter in string:
+        index = string.index(delimiter)
+        positive = string[:index].rstrip()
+        negative = string[index+len(delimiter):].lstrip()
+        return positive, negative
+    else:
+        return string, "null" #if we are missing the ng bad thing happen....
+
 @app.on_message(filters.command(["draw"]))
 def draw(client, message):
     msgs = message.text.split(' ', 1)
     if len(msgs) == 1:
-        message.reply_text("Format : /draw < text to image >")
+        message.reply_text("Format :\n/draw < text to image >\nng < negative (optional) >")
         return
-    msg = msgs[1]
+    
+
+    msg = slice_positive_negative(msgs[1])
+    print(msg)
+    payload = {
+                "prompt": msg[0],
+                 "negative_prompt": msg[1],
+               }
+    
+    print(payload)
 
     K = message.reply_text("Please Wait 10-15 Second")
-
-    payload = {"prompt": msg}
-
     r = requests.post(url=f'{SD_URL}/sdapi/v1/txt2img', json=payload).json()
 
     # explaination
@@ -77,9 +93,9 @@ def draw(client, message):
         message.reply_photo(
             photo=f"{word}.png",
             caption=
-            f"Prompt - **{msg}**\n **[{message.from_user.first_name}-Kun](tg://user?id={message.from_user.id})**"
+            f"Prompt - **{msg[0]}**\nNegative Prompt - **{msg[1]}**\n**[{message.from_user.first_name}-Kun](tg://user?id={message.from_user.id})**"
         )
-        os.remove(f"{word}.png")
+        # os.remove(f"{word}.png")
         K.delete()
 
 
